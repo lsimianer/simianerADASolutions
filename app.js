7436c49fe89db7fd2ba347b2db14193d11a50a37
Lukas Simianer <lsimianer@gmail.com>
2020-07-24 16:51:43 -0500

    maginc

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 
 
-const mailShit = () => {
-    window.open('mailto:lsimianer@codehounds.io?subject=ADA%20consultation%20request&body=Please%20answer%20the%20following%20questions%20without%20deleting%20the%20prompts.%0D%0A%0D%0AWhat%20is%20your%20industry%3A%0D%0A%0D%0Aestimated%20%25%20of%20customers%20with%20sight%20disability%3A%0D%0A%0D%0Awhat%20does%20your%20digital%20presence%20do%20for%20you%3A%0D%0A%0D%0Ado%20you%20have%20a%20timeline%20of%20desired%20ADA%20compliance%3A%0D%0A%0D%0Abest%20contact%20phone%20number%3A%0D%0A%0D%0Abest%20contact%20email%3A%0D%0A%0D%0Apreferred%20contact%20method%3A%0D%0A%0D%0AThank%20you%20for%20choosing%20Simianer%20ADA%20solutions.%0D%0A%0D%0A');
 
+const toggleText = () => {
+    let myDom = document.querySelectorAll('.instructional')
+
+    for(var i = 0; i < myDom.length; i++){
+        myDom[i].classList.toggle('biggerText')
+    }
 }
+
+const toggleContrast = () => {
+    let myDom = document.querySelectorAll('.instructional')
+
+    for(var i = 0; i < myDom.length; i++){
+        myDom[i].classList.toggle('Contrast')
+    }
+}
+
+const toggleBorder = () => {
+    let myDom = document.querySelectorAll('.instructional')
+
+    for(var i = 0; i < myDom.length; i++){
+        myDom[i].classList.toggle('border')
+    }
+}
+
