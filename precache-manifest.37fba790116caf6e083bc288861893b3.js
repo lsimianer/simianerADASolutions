964aae6499cbce679dd585f199be6a70b99abb13
Lukas Simianer <lsimianer@gmail.com>
2020-12-16 00:35:18 -0600

    Updates

---
+++ precache-manifest.37fba790116caf6e083bc288861893b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6d22b4a8782d55ac68842adb4a9226",
+    "url": "/simianerADASolutions/index.html"
+  },
+  {
+    "revision": "9ac367407c166d396e83",
+    "url": "/simianerADASolutions/static/css/main.51e66f0b.chunk.css"
+  },
+  {
+    "revision": "2d2a913dff6faae1ba85",
+    "url": "/simianerADASolutions/static/js/2.bb014903.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/simianerADASolutions/static/js/2.bb014903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac367407c166d396e83",
+    "url": "/simianerADASolutions/static/js/main.837ec7a8.chunk.js"
+  },
+  {
+    "revision": "f893f7673d914057a492",
+    "url": "/simianerADASolutions/static/js/runtime-main.936b0643.js"
+  },
+  {
+    "revision": "9d422bd219010c5a1f3abceb0c116abc",
+    "url": "/simianerADASolutions/static/media/REB2.9d422bd2.png"
+  },
+  {
+    "revision": "307827eca64e18f20cba90b751fb9b48",
+    "url": "/simianerADASolutions/static/media/govt.307827ec.jpeg"
+  },
+  {
+    "revision": "1b3aacd578d5807f944676a7e64c100e",
+    "url": "/simianerADASolutions/static/media/shreddedPaper.1b3aacd5.jpg"
+  }
+]);
